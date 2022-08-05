4fed322cf199cc8b0f5e9a9080bf3db7cee76625
Gregory Edison <gregory.edison1993@gmail.com>
2022-08-05 18:37:32 +0200

    test: position cases

---
+++ test/PositionTestCases.ts
@@ -0,0 +1,295 @@
+interface TestCasePosition {
+    description: string
+    price: bigint
+    amount: bigint
+    feeBps: bigint
+    error: string[]
+}
+
+const POSITION_BASE_TEST_CASE: TestCasePosition[] = [
+    {
+        description: 'price 0, amount 0, feeBps 0',
+        price: BigInt(0),
+        amount: BigInt(0),
+        feeBps: BigInt(0),
+        error: [],
+    },
+    {
+        description: 'price 1500, amount 0, feeBps 0',
+        price: BigInt(1_500),
+        amount: BigInt(0),
+        feeBps: BigInt(0),
+        error: [],
+    },
+    {
+        description: 'price 0, amount 10, feeBps 0',
+        price: BigInt(0),
+        amount: BigInt(10),
+        feeBps: BigInt(0),
+        error: [],
+    },
+    {
+        description: 'price 0, amount 0, feeBps 100',
+        price: BigInt(0),
+        amount: BigInt(0),
+        feeBps: BigInt(100),
+        error: [],
+    },
+    {
+        description: 'price 1500, amount 10, feeBps 0',
+        price: BigInt(1_500),
+        amount: BigInt(10),
+        feeBps: BigInt(0),
+        error: [],
+    },
+    {
+        description: 'price 0, amount 10, feeBps 100',
+        price: BigInt(0),
+        amount: BigInt(10),
+        feeBps: BigInt(100),
+        error: [],
+    },
+    {
+        description: 'price 1500, amount 0, feeBps 100',
+        price: BigInt(1_500),
+        amount: BigInt(0),
+        feeBps: BigInt(100),
+        error: [],
+    },
+    {
+        description: 'price 1500, amount 10, feeBps 100',
+        price: BigInt(1_500),
+        amount: BigInt(10),
+        feeBps: BigInt(100),
+        error: [],
+    },
+    // negative values
+    {
+        description: 'price 0, amount 0, feeBps 0',
+        price: BigInt(0),
+        amount: BigInt(0),
+        feeBps: BigInt(0),
+        error: [],
+    },
+    {
+        description: 'price 1500, amount 0, feeBps 0',
+        price: BigInt(1_500),
+        amount: BigInt(0),
+        feeBps: BigInt(0),
+        error: [],
+    },
+    {
+        description: 'price 0, amount -10, feeBps 0',
+        price: BigInt(0),
+        amount: BigInt(-10),
+        feeBps: BigInt(0),
+        error: [],
+    },
+    {
+        description: 'price 0, amount 0, feeBps 100',
+        price: BigInt(0),
+        amount: BigInt(0),
+        feeBps: BigInt(100),
+        error: [],
+    },
+    {
+        description: 'price 1500, amount -10, feeBps 0',
+        price: BigInt(1_500),
+        amount: BigInt(-10),
+        feeBps: BigInt(0),
+        error: [],
+    },
+    {
+        description: 'price 0, amount -10, feeBps 100',
+        price: BigInt(0),
+        amount: BigInt(-10),
+        feeBps: BigInt(100),
+        error: [],
+    },
+    {
+        description: 'price 1500, amount 0, feeBps 100',
+        price: BigInt(1_500),
+        amount: BigInt(0),
+        feeBps: BigInt(100),
+        error: [],
+    },
+    {
+        description: 'price 1500, amount -10, feeBps 100',
+        price: BigInt(1_500),
+        amount: BigInt(-10),
+        feeBps: BigInt(100),
+        error: [],
+    },
+]
+
+const POSITION_REVERT_TEST_CASE: TestCasePosition[][] = [
+    // [
+    //     {
+    //         description:
+    //             'price 2**64, amount 2**65, feeBps 0, fails on abs_value',
+    //         price: BigInt(2 ** 64),
+    //         amount: BigInt(2 ** 65),
+    //         feeBps: BigInt(0),
+    //         error: [
+    //             'AssertionError: value=680564733841876926926749214863536422912 is out of the valid range.',
+    //         ],
+    //     },
+    // ],
+    // [
+    //     {
+    //         description:
+    //             'price 2**65, amount 2**64, feeBps 0, fails on abs_value',
+    //         price: BigInt(2 ** 65),
+    //         amount: BigInt(2 ** 64),
+    //         feeBps: BigInt(0),
+    //         error: [
+    //             'AssertionError: value=680564733841876926926749214863536422912 is out of the valid range.',
+    //         ],
+    //     },
+    // ],
+    // [
+    //     {
+    //         description:
+    //             'price 1, amount 2**82 + 1, feeBps 0, fails on positive amount',
+    //         price: BigInt(1),
+    //         amount: BigInt(2 ** 82) + BigInt(1),
+    //         feeBps: BigInt(0),
+    //         error: [
+    //             'Value 3618502788666131213697322783095070105623107215331596699973092056135872020479, in range check builtin 8, is out of range [0, 340282366920938463463374607431768211456).',
+    //         ],
+    //     },
+    // ],
+    // [
+    //     {
+    //         description:
+    //             'price 1, -amount 2**82 - 1, feeBps 0, fails on negative amount',
+    //         price: BigInt(1),
+    //         amount: -BigInt(2 ** 82) - BigInt(1),
+    //         feeBps: BigInt(0),
+    //         error: [
+    //             'Value 3618502788666131213697322783095070105623107215331596699973092056135872020480, in range check builtin 7, is out of range [0, 340282366920938463463374607431768211456).',
+    //         ],
+    //     },
+    // ],
+    // [
+    //     {
+    //         description: 'price 10**13, amount 1, feeBps 1, fails on price',
+    //         price: BigInt(10 ** 13),
+    //         amount: BigInt(1),
+    //         feeBps: BigInt(1),
+    //         error: [
+    //             'Value 3618502788666131213697322783095070105623107215331596699973092056135872020480, in range check builtin 10, is out of range [0, 340282366920938463463374607431768211456).',
+    //         ],
+    //     },
+    // ],
+    [
+        {
+            description:
+                'price 1, amount 2**81, feeBps 1, runs twice and fails on positive size',
+            price: BigInt(1),
+            amount: BigInt(2 ** 81),
+            feeBps: BigInt(1),
+            error: [],
+        },
+        {
+            description:
+                'price 1, amount 2**81, feeBps 1, runs twice and fails on positive size',
+            price: BigInt(1),
+            amount: BigInt(2 ** 81),
+            feeBps: BigInt(1),
+            error: [
+                'Value 3618502788666131213697322783095070105623107215331594282121452826877522608130, in range check builtin 12, is out of range [0, 340282366920938463463374607431768211456).',
+            ],
+        },
+    ],
+    [
+        {
+            description:
+                'price 1, amount -2**81, feeBps 1, runs twice and fails on negative size',
+            price: BigInt(1),
+            amount: -BigInt(2 ** 81),
+            feeBps: BigInt(1),
+            error: [],
+        },
+        {
+            description:
+                'price 1, amount -2**81, feeBps 1, runs twice and fails on negative size',
+            price: BigInt(1),
+            amount: -BigInt(2 ** 81),
+            feeBps: BigInt(1),
+            error: [
+                'Value 3618502788666131213697322783095070105623107215331596699973092056135872020480, in range check builtin 12, is out of range [0, 340282366920938463463374607431768211456).',
+            ],
+        },
+    ],
+    [
+        {
+            description:
+                'price 10**13-1, amount 2**82 - 1, feeBps 1, runs 4 times and fails on positive cost',
+            price: BigInt(10 ** 13) - BigInt(1),
+            amount: BigInt(2 ** 82) - BigInt(1),
+            feeBps: BigInt(1),
+            error: [],
+        },
+        {
+            description:
+                'price 10**13-1, amount 2**82 - 1, feeBps 1, runs 4 times and fails on positive cost',
+            price: BigInt(10 ** 13) - BigInt(1),
+            amount: BigInt(2 ** 82) - BigInt(1),
+            feeBps: BigInt(1),
+            error: [],
+        },
+        {
+            description:
+                'price 10**13-1, amount 2**82 - 1, feeBps 1, runs 4 times and fails on positive cost',
+            price: BigInt(10 ** 13) - BigInt(1),
+            amount: BigInt(2 ** 82) - BigInt(1),
+            feeBps: BigInt(1),
+            error: [],
+        },
+        {
+            description:
+                'price 10**13-1, amount 2**82 - 1, feeBps 1, runs 4 times and fails on positive cost',
+            price: BigInt(10 ** 13) - BigInt(1),
+            amount: BigInt(2 ** 82) - BigInt(1),
+            feeBps: BigInt(1),
+            error: ['cost'],
+        },
+    ],
+    [
+        {
+            description:
+                'price 10**13-1, amount -2**82 + 1, feeBps 1, runs 4 times and fails on negative cost',
+            price: BigInt(10 ** 13) - BigInt(1),
+            amount: -BigInt(2 ** 82) + BigInt(1),
+            feeBps: BigInt(1),
+            error: [''],
+        },
+        {
+            description:
+                'price 10**13-1, amount -2**82 + 1, feeBps 1, runs 4 times and fails on negative cost',
+            price: BigInt(10 ** 13) - BigInt(1),
+            amount: -BigInt(2 ** 82) + BigInt(1),
+            feeBps: BigInt(1),
+            error: [''],
+        },
+        {
+            description:
+                'price 10**13-1, amount -2**82 + 1, feeBps 1, runs 4 times and fails on negative cost',
+            price: BigInt(10 ** 13) - BigInt(1),
+            amount: -BigInt(2 ** 82) + BigInt(1),
+            feeBps: BigInt(1),
+            error: [''],
+        },
+        {
+            description:
+                'price 10**13-1, amount -2**82 + 1, feeBps 1, runs 4 times and fails on negative cost',
+            price: BigInt(10 ** 13) - BigInt(1),
+            amount: -BigInt(2 ** 82) + BigInt(1),
+            feeBps: BigInt(1),
+            error: ['cost'],
+        },
+    ],
+]
+
+export { TestCasePosition, POSITION_BASE_TEST_CASE, POSITION_REVERT_TEST_CASE }
--- test/PositionsTest.ts
+++ test/PositionsTest.ts
@@ -5,9 +5,24 @@ import {
 } from 'hardhat/types/runtime'
 import { expect } from 'chai'
 import { starknet } from 'hardhat'
+import {
+    POSITION_BASE_TEST_CASE,
+    POSITION_REVERT_TEST_CASE,
+    TestCasePosition,
+} from './PositionTestCases'
 
 let contract: StarknetContract
-let address: BigInt
+let address: bigint
+let prime: bigint
+
+let maxPrice: bigint = BigInt(10 ** 13)
+let rangeCheckBound: bigint = BigInt(2 ** 128)
+let maxAmount: bigint = BigInt(2 ** 82)
+let maxBound: bigint = BigInt(2 ** 127)
+
+function abs(num: bigint): bigint {
+    return num < 0n ? -num : num
+}
 
 async function getPosition(address: BigInt) {
     const args: StringMap = { address: address }
@@ -17,11 +32,12 @@ async function getPosition(address: BigInt) {
 
 before(async () => {
     const contractFactory: StarknetContractFactory =
-        await starknet.getContractFactory('test/Positions_test.cairo')
+        await starknet.getContractFactory('test/positions_test.cairo')
     contract = await contractFactory.deploy()
     address = BigInt(
         '0x7cde936f47a2240ab1f8764f4dcce14b53af1a5751c33eb4ecbfd643239da5d'
     )
+    prime = BigInt(2 ** 251) + BigInt(17) * BigInt(2 ** 192) + BigInt(1)
 })
 
 describe('#update', () => {
@@ -31,20 +47,65 @@ describe('#update', () => {
         expect(pos.position.cost).to.equal(0n)
         expect(pos.position.size).to.equal(0n)
     })
-    it('should pass with an update of the position', async () => {
-        const price = BigInt(1_500_000_000) // price in USDC with 6 decimals
-        const amount = BigInt(1)
-        const feeBps = BigInt(100) // fees in bips
-        const args: StringMap = {
-            address: address,
-            price: price,
-            amount: amount,
-            feeBps: feeBps,
+
+    // it('should pass with for each base case', async () => {
+    //     for (const baseCase of POSITION_BASE_TEST_CASE) {
+    //         const args: StringMap = {
+    //             address: address,
+    //             price: baseCase.price,
+    //             amount: baseCase.amount,
+    //             feeBps: baseCase.feeBps,
+    //         }
+    //         await contract.invoke('update_test', args)
+    //         const pos = await getPosition(address)
+    //         const cost = baseCase.amount * baseCase.price
+    //         const fees = (abs(cost) * baseCase.feeBps) / 10_000n
+    //         expect(pos.position.fees).to.equal(fees, 'failed on fees')
+    //         expect(pos.position.cost).to.equal(cost, 'failed on cost')
+    //         expect(pos.position.size).to.equal(
+    //             baseCase.amount,
+    //             'failed on size'
+    //         )
+    //         const arg: StringMap = {
+    //             address: address,
+    //             price: 0,
+    //         }
+    //         await contract.invoke('settle_test', arg)
+    //     }
+    // })
+
+    it('should fail for each base case', async () => {
+        for (const failScenario of POSITION_REVERT_TEST_CASE) {
+            let args: StringMap
+            let index: number = 0
+            for (let i = 0; failScenario[i].error.length == 0; i++) {
+                args = {
+                    address: address,
+                    price: failScenario[i].price,
+                    amount: failScenario[i].amount,
+                    feeBps: failScenario[i].feeBps,
+                }
+                await contract.invoke('update_test', args)
+                index = i + 1
+            }
+            try {
+                args = {
+                    address: address,
+                    price: failScenario[index].price,
+                    amount: failScenario[index].amount,
+                    feeBps: failScenario[index].feeBps,
+                }
+                await contract.invoke('update_test', args)
+                expect.fail(
+                    'should have failed with',
+                    failScenario[index].description
+                )
+            } catch (error: any) {
+                expect(error.message).to.contain(
+                    failScenario[index].error,
+                    failScenario[index].description
+                )
+            }
         }
-        await contract.invoke('update_test', args)
-        const pos = await getPosition(address)
-        expect(pos.position.fees).to.equal(15_000_000n)
-        expect(pos.position.cost).to.equal(1_500_000_000n)
-        expect(pos.position.size).to.equal(1n)
     })
 })
