29a00de4855b9efc0d3d2995943a98ec5e14b283
Gregory Edison <gregory.edison1993@gmail.com>
2022-11-04 10:02:15 -0700

    chore: data cleaning

--- test/integration/utils/clean.ts
+++ test/integration/utils/clean.ts
@@ -1,80 +1,92 @@
 import fs from 'fs'
 import { Operation, operation } from '../utils/types'
-import data from '../data/data.json'
+import untypedData from '../data/data.json'
 import clean_data from '../data/data_cleaned.json'
 
 const LIMIT = 3000
+
+let openPositions = new Map<bigint, boolean>()
+const data: any[] = untypedData
 
 clean()
 
 function clean() {
     let myData: Operation[] = []
-    for (let i = 0; i < LIMIT; i++) {
-        let block = BigInt(data[i]['block-number'])
-        console.log(`At iteration ${i}`)
-        if (data[i]['open-position'] !== null) {
-            let oracle_price = BigInt(
-                data[i]['open-position']!['oracle-price']!
-            )
-            let margin = BigInt(data[i]['open-position']!['margin']!)
-            let price = BigInt(data[i]['open-position']!['price']!)
-            let leverage = BigInt(data[i]['open-position']!['leverage']!)
+    for (const event of data) {
+        let block = BigInt(event['block-number'])
+        if (
+            event['open-position'] !== null &&
+            event['open-position']['product-id'] == 2
+        ) {
+            console.log('ADDED OPEN')
+            let oracle_price = BigInt(event['open-position']['oracle-price'])
+            let margin = BigInt(event['open-position']['margin'])
+            let price = BigInt(event['open-position']['price'])
+            let leverage = BigInt(event['open-position']['leverage'])
             let notional = margin * leverage // (precision 16)
             let amount = notional / price / BigInt(100) // (precision 6)
+            const positionId = BigInt(event['open-position']['position-id'])
             myData.push({
-                user: data[i]['open-position']!.user,
-                positionId: BigInt(data[i]['open-position']!['position-id']!),
+                user: event['open-position'].user,
+                positionId: positionId,
                 amount: amount,
                 margin: margin / BigInt(100),
-                price: price / BigInt(100),
+                price: oracle_price / BigInt(100),
                 isLong: true,
                 block: block,
                 type: operation.OpenPosition,
             })
+            openPositions.set(positionId, true)
         }
-        if (data[i]['close-position'] !== null) {
-            let price = BigInt(data[i]['close-position']!['price']!)
+        if (
+            event['close-position'] !== null &&
+            openPositions.has(BigInt(event['close-position']['position-id']))
+        ) {
+            console.log('ADDED CLOSE')
+            let price = BigInt(event['close-position']['price'])
             myData.push({
-                user: data[i]['close-position']!.user,
-                positionId: BigInt(data[i]['close-position']!['position-id']!),
+                user: event['close-position'].user,
+                positionId: BigInt(event['close-position']['position-id']),
                 price: price / BigInt(100),
                 block: block,
                 type: operation.ClosePosition,
             })
         }
-        if (data[i]['staked'] !== null) {
+        if (event['staked'] !== null) {
             myData.push({
-                user: data[i]['staked']!.user,
-                amount: BigInt(data[i]['staked']!.amount) / 100n,
+                user: event['staked'].user,
+                amount: BigInt(event['staked'].amount) / 100n,
                 block: block,
                 type: operation.ProvideLiquidity,
             })
         }
-        if (data[i]['redeemed'] !== null) {
+        if (event['redeemed'] !== null) {
             myData.push({
-                user: data[i]['redeemed']!.user,
-                amount: BigInt(data[i]['redeemed']!.amount) / 100n,
+                user: event['redeemed'].user,
+                amount: BigInt(event['redeemed'].amount) / 100n,
                 block: block,
                 type: operation.RemoveLiquidity,
             })
         }
-        if (data[i]['add-margin'] !== null) {
+        if (event['add-margin'] !== null) {
             myData.push({
-                user: data[i]['add-margin']!.user,
-                amount: BigInt(data[i]['add-margin']!.margin) / 100n,
+                user: event['add-margin'].user,
+                amount: BigInt(event['add-margin'].margin) / 100n,
                 block: block,
                 type: operation.AddCollateral,
             })
         }
-        if (data[i]['position-liquidated'] !== null) {
+        if (
+            event['position-liquidated'] !== null &&
+            openPositions.has(event['position-liquidated']['position-id'])
+        ) {
             myData.push({
-                positionId: BigInt(
-                    data[i]['position-liquidated']!['position-id']!
-                ),
+                positionId: BigInt(event['position-liquidated']['position-id']),
                 block: block,
                 type: operation.Liquidate,
             })
         }
+        if (myData.length == 3000) break
     }
     const jsonData = JSON.stringify(myData, (_, value) => {
         return typeof value === 'bigint' ? value.toString() : value
